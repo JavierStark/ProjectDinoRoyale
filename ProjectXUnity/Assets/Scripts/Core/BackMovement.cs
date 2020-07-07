bf5f55fd9d5a7aa80d031d817d5a2573363ee718
JavierStark <JavierTorralboCortes@gmail.com>
2020-07-07 18:29:08 +0200

    Suelo que se desplaza
    Se ha implementado una clase para controlar el movimiento del suelo y el fondo cuando exista

---
+++ ProjectXUnity/Assets/Scripts/Core/BackMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runner.Core {
+    public class BackMovement : MonoBehaviour {
+
+        [Range(0,0.09f)][SerializeField] float soilVelocity;
+        RawImage soil;
+
+        private void Start() {
+            soil = GetComponent<RawImage>();
+        }
+        private void Update() {
+            
+            soil.uvRect = new Rect(soil.uvRect.position + new Vector2(soilVelocity,0), soil.uvRect.size);
+        }
+    }
+}
