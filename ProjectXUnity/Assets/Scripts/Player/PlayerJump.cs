4deb27b9826ec20be07420a70fabe98d5e19e6e8
JavierStark <JavierTorralboCortes@gmail.com>
2020-07-07 10:08:28 +0200

    Salto añadido
    Se ha añadido rb2D al dino.
    También se ha añadido el salto completamente editable desde inspector.
    Se ha cambiado la gravedad global para mejorar el salto.

---
+++ ProjectXUnity/Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Numerics;
+using UnityEditor;
+using UnityEngine;
+
+namespace Runner.Player {
+    public class PlayerJump : MonoBehaviour {
+
+        private float jump;
+
+        [Range(1, 30)] [SerializeField] float jumpForce;
+        Rigidbody2D playerRB;
+
+        [Header("Ground Checker")]
+        [SerializeField] UnityEngine.Vector2 checkerSize;
+        [SerializeField] Transform checkerPosition;
+        [SerializeField] LayerMask groundMask;
+
+
+        private void Start() {
+            playerRB = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update() {
+            jump = Input.GetAxis("Jump");
+            if (IsGrounded() && jump > 0) {
+                Jump();
+            }
+        }
+        private bool IsGrounded() {
+            Debug.Log("Ground");
+            bool isGrounded = Physics2D.OverlapBox(checkerPosition.position, checkerSize, 0, groundMask);
+            return isGrounded;
+        }
+
+        private void Jump() {
+            playerRB.velocity = new UnityEngine.Vector2(0, jumpForce);
+        }
+
+        //Draws the ground checker
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.grey;
+            Gizmos.DrawWireCube(checkerPosition.position, checkerSize);
+        }
+    }
+}
