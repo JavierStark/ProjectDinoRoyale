fd6b967490a0b960c0ab5ef56d0429b7e390f50e
JavierStark <JavierTorralboCortes@gmail.com>
2020-07-14 10:09:45 +0200

    Omg que no se había guardado todo XD

--- ProjectXUnity/Assets/Scripts/Core/FileManager.cs
+++ ProjectXUnity/Assets/Scripts/Core/FileManager.cs
@@ -8,20 +8,9 @@ public class FileManager : MonoBehaviour
 {
     string namesFilePath = "F:/UnityProyectos/ProjectX/ProjectX/ProjectXUnity/Assets/Names.txt";
 
-    private void Awake() {
-        Debug.Log(namesFilePath);
-    }
-
-    private void Start() {
-        ReadFile();
-    }
-
-
     public string[] ReadFile() {
         string[] namesInFile = File.ReadAllLines(namesFilePath);
-        foreach(string name in namesInFile) {
-            print(name);
-        }
+
         return namesInFile;
     }
 }
