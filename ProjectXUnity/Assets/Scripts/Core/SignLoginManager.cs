b0076abdffe96fafa23587ad61fcfdceb3e20815
rubenmorcillo <ravent953@gmail.com>
2020-07-22 20:02:41 +0200

    Paneles de login y signup

---
+++ ProjectXUnity/Assets/Scripts/Core/SignLoginManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class SignLoginManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject panelLogin, panelSign;
+    TMP_Text txtInfo;
+    void Start()
+    {
+       panelLogin = GameObject.Find("PanelLogin");
+       panelSign = GameObject.Find("PanelSign");
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckConfirmPassword()
+	{
+        string pass = null;
+        string confirmPass = null;
+        TMP_InputField[] inputs = panelSign.GetComponentsInChildren<TMP_InputField>();
+        foreach(TMP_InputField i in inputs)
+		{
+            if (i.name == "InputPassword")
+			{
+                pass = i.text;
+
+			}else if (i.name == "InputConfirmPassword")
+            {
+                confirmPass = i.text;
+			}
+		}
+        if (pass == confirmPass)
+		{
+            Debug.Log("son  iguales");
+		}
+		else
+		{
+            Debug.Log("no son iguales");
+		}
+	}
+}
