9333b6a460542bde9d542b8dc8e049d87f320247
JavierStark <JavierTorralboCortes@gmail.com>
2020-08-03 19:51:40 +0200

    Ahora no se mueven XD

---
+++ ProjectXUnity/Assets/Scripts/Core/FeedbackButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackButton : MonoBehaviour
+{
+    public void ToPlayStore() {
+
+        Application.OpenURL("market://details?id=" + Application.identifier);
+       
+    }
+}
